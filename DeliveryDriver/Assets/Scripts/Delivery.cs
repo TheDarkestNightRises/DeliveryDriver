c4c788d8206d02fdfb843a9b7cb49a0e6d36b6d9
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-08-19 18:57:24 +0200

    Added pickups , boosters, bumps

--- DeliveryDriver/Assets/Scripts/Delivery.cs
+++ DeliveryDriver/Assets/Scripts/Delivery.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32 (1,1,1,1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1,1,1,1);  
+    [SerializeField] float destroyDelay = 0.5f;
+    bool hasPackage = false;
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Lol");
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("What was that ?");
+       if (other.tag == "Package" && !hasPackage)
+       {
+            Debug.Log("Package picked up");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
+       }
+       if (other.tag == "Customer" && hasPackage)
+       {
+            Debug.Log("Package delivered");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+
+        }
     }
 }
--- DeliveryDriver/Assets/Scripts/Driver.cs
+++ DeliveryDriver/Assets/Scripts/Driver.cs
@@ -6,6 +6,8 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 300f;
     [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float bootSpeed = 30f;
     void Start()
     {
     }
@@ -18,5 +20,18 @@ public class Driver : MonoBehaviour
 
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0,moveAmount,0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Boost")
+        {
+            moveSpeed = bootSpeed;
+        }
     }
 }
